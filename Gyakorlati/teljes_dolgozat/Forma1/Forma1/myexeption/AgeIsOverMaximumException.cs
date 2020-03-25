c6354c2c5def333f72cbbbeddc26a80607f43383
MiklosArpad <43209274+MiklosArpad@users.noreply.github.com>
2020-03-25 15:59:55 +0100

    Kivétel kezelés

---
+++ Gyakorlati/teljes_dolgozat/Forma1/Forma1/myexeption/AgeIsOverMaximumException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Forma1.myexeption
+{
+    [Serializable]
+    internal class AgeIsOverMaximumException : Exception
+    {
+        public AgeIsOverMaximumException()
+        {
+        }
+
+        public AgeIsOverMaximumException(string message) : base(message)
+        {
+        }
+
+        public AgeIsOverMaximumException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AgeIsOverMaximumException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Gyakorlati/teljes_dolgozat/Forma1/Forma1/myexeption/AgeIsUnderMinimumException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Forma1.myexeption
+{
+    [Serializable]
+    internal class AgeIsUnderMinimumException : Exception
+    {
+        public AgeIsUnderMinimumException()
+        {
+        }
+
+        public AgeIsUnderMinimumException(string message) : base(message)
+        {
+        }
+
+        public AgeIsUnderMinimumException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AgeIsUnderMinimumException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
